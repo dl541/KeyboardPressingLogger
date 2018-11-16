d9ecf8b283119e27ac2fcbaa160b52bc8e494e7e
dl541 <dl541@cam.ac.uk>
2018-11-16 00:09:32 +0000

    Add optimized keyboard activity, implemented on touch listener

---
+++ app/src/main/java/com/example/android/justjava/OptimizedKeyboardActivity.java
@@ -0,0 +1,39 @@
+package com.example.android.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class OptimizedKeyboardActivity extends AppCompatActivity {
+
+    private View optimizedKeyboardBaseView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.optimized_keyboard);
+        optimizedKeyboardBaseView = (View) findViewById(R.id.optimizedKeyboardBase);
+        optimizedKeyboardBaseView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+
+                if (event.getAction() == MotionEvent.ACTION_DOWN ||
+                        event.getAction() == MotionEvent.ACTION_UP ||
+                        event.getAction() == MotionEvent.ACTION_POINTER_DOWN ||
+                        event.getAction() == MotionEvent.ACTION_POINTER_UP) {
+
+                    //First flag: 0 represents coordinate message
+                    //Second flag: 0 represents pressed 1 represents released
+                    String log = String.format("%s\t%s\t%s", event.getAction(), event.getX(), event.getY());
+                    Log.i("OptimizedKeyboard", log);
+
+                }
+                return true;
+            }
+        });
+    }
+}
